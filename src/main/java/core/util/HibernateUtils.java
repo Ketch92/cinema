ccc7b9ff950fac66f885130e95dae88be4c905b4
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-26 15:31:14 +0200

    Implemented HibernateUtils

--- src/main/java/core/util/HibernateUtils.java
+++ src/main/java/core/util/HibernateUtils.java
@@ -1,4 +1,20 @@
 package core.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtils {
+    private static SessionFactory sessionFactory = initSessionFactory();
+    
+    private static SessionFactory initSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Exception e) {
+            throw new RuntimeException("Errored while creating the SessionFactory");
+        }
+    }
+    
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
