6009442ec20d48185b670bf1d304946faaf0bd68
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-26 15:19:44 +0200

    added hibernate entity and Id annotations

--- src/main/java/core/model/Movie.java
+++ src/main/java/core/model/Movie.java
@@ -1,8 +1,15 @@
 package core.model;
 
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
