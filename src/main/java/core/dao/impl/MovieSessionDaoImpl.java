933c9f5b87335d2d38a98eeb29b4209a9dc1d6c8
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-31 17:13:35 +0200

    Implemented MovieSessionDaoImpl.java and CinemaHallServiceImpl.java

--- src/main/java/core/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/core/dao/impl/MovieSessionDaoImpl.java
@@ -1,18 +1,25 @@
 package core.dao.impl;
 
 import core.dao.MovieSessionDao;
+import core.lib.Inject;
+import core.lib.Service;
 import core.model.MovieSession;
 import java.time.LocalDate;
 import java.util.List;
 
+@Service
 public class MovieSessionDaoImpl implements MovieSessionDao {
+    
+    @Inject
+    private MovieSessionDao movieSessionDao;
+    
     @Override
     public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
-        return null;
+        return movieSessionDao.findAvailableSessions(movieId, date);
     }
     
     @Override
     public MovieSession add(MovieSession session) {
-        return null;
+        return movieSessionDao.add(session);
     }
 }
--- src/main/java/core/service/impl/CinemaHallServiceImpl.java
+++ src/main/java/core/service/impl/CinemaHallServiceImpl.java
@@ -1,17 +1,25 @@
 package core.service.impl;
 
+import core.dao.CinemaHallDao;
+import core.lib.Inject;
+import core.lib.Service;
 import core.model.CinemaHall;
 import core.service.CinemaHallService;
 import java.util.List;
 
+@Service
 public class CinemaHallServiceImpl implements CinemaHallService {
+    
+    @Inject
+    private CinemaHallDao cinemaHallDao;
+    
     @Override
     public CinemaHall add(CinemaHall cinemaHall) {
-        return null;
+        return cinemaHallDao.add(cinemaHall);
     }
     
     @Override
     public List<CinemaHall> getAll() {
-        return null;
+        return cinemaHallDao.getAll();
     }
 }
