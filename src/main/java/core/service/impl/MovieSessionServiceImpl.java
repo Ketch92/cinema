aee7b80e3177a648c04582aca76783d1ce6daf71
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-31 21:05:32 +0200

    Refactored MovieSessionServiceImpl.java

--- src/main/java/core/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/core/service/impl/MovieSessionServiceImpl.java
@@ -1,18 +1,26 @@
 package core.service.impl;
 
+import core.dao.MovieSessionDao;
+import core.lib.Inject;
+import core.lib.Service;
 import core.model.MovieSession;
 import core.service.MovieSessionService;
 import java.time.LocalDate;
 import java.util.List;
 
+@Service
 public class MovieSessionServiceImpl implements MovieSessionService {
+    
+    @Inject
+    private MovieSessionDao movieSessionDao;
+    
     @Override
     public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
-        return null;
+        return movieSessionDao.findAvailableSessions(movieId, date);
     }
     
     @Override
     public MovieSession add(MovieSession session) {
-        return null;
+        return movieSessionDao.add(session);
     }
 }
