7104cf8d96bc1b0345ceff5fd137ab6d98cc0dca
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-31 20:39:24 +0200

    Implemented methods in CinemaHallDaoImpl.java

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,7 +1,9 @@
 package core;
 
 import core.lib.Injector;
+import core.model.CinemaHall;
 import core.model.Movie;
+import core.service.CinemaHallService;
 import core.service.MovieService;
 
 public class Main {
@@ -9,9 +11,17 @@ public class Main {
     
     public static void main(String[] args) {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        CinemaHallService cinemaHallService = (CinemaHallService) injector.getInstance(CinemaHallService.class);
+        
         Movie movie = new Movie();
         movie.setTitle("Fast and Furious");
         movieService.add(movie);
         movieService.getAll().forEach(System.out::println);
+    
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(100);
+        cinemaHall.setDescription("4DX big hole");
+        cinemaHallService.add(cinemaHall);
+        cinemaHallService.getAll().forEach(System.out::println);
     }
 }
