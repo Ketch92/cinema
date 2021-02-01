51bdd5b1515143c88a733a604b9d4b59c4bdf8d9
Oleh Kravets <oleg.kravets92@gmail.com>
2021-02-01 19:25:02 +0200

    Implemented AuthenticationUtils.java

---
+++ src/main/java/core/util/AuthenticationUtils.java
@@ -0,0 +1,32 @@
+package core.util;
+
+import core.model.exception.InitializationException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import javax.xml.crypto.dsig.DigestMethod;
+
+public class AuthenticationUtils {
+    
+    public static String hashPassword(String password, byte[] salt) {
+        StringBuilder hashedPassword = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(DigestMethod.SHA512);
+            messageDigest.update(salt);
+            byte[] digest = messageDigest.digest(password.getBytes());
+            for (byte b : digest) {
+                hashedPassword.append(String.format("%02x", b));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new InitializationException("Can't provide hashed password due to ", e);
+        }
+        return hashedPassword.toString();
+    }
+    
+    public static byte[] getSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[32];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+}
