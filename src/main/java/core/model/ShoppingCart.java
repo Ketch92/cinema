878f454edbd106c34ab9d608b02b7a6046325f54
Oleh Kravets <oleg.kravets92@gmail.com>
2021-02-03 16:46:12 +0200

    Added Ticket.java and ShoppingCart.java

---
+++ src/main/java/core/model/ShoppingCart.java
@@ -0,0 +1,75 @@
+package core.model;
+
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shopping_cart")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToMany
+    private List<Ticket> ticketList;
+    @OneToOne
+    private User user;
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public List<Ticket> getTicketList() {
+        return ticketList;
+    }
+    
+    public void setTicketList(List<Ticket> ticketList) {
+        this.ticketList = ticketList;
+    }
+    
+    public User getUser() {
+        return user;
+    }
+    
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    @Override
+    public String toString() {
+        return "ShoppingCart{"
+               + "id=" + id
+               + ", ticketList=" + ticketList
+               + ", user=" + user.getEmail()
+               + '}';
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ShoppingCart that = (ShoppingCart) o;
+        return Objects.equals(id, that.id)
+               && Objects.equals(ticketList, that.ticketList)
+               && Objects.equals(user, that.user);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, ticketList, user);
+    }
+}
