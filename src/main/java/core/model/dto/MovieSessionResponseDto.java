54ecd23f60dfdcb67998c0241caa576cba067f16
Oleh Kravets <oleg.kravets92@gmail.com>
2021-02-12 18:28:47 +0200

    Added DTOs

---
+++ src/main/java/core/model/dto/MovieSessionResponseDto.java
@@ -0,0 +1,77 @@
+package core.model.dto;
+
+import java.time.LocalDateTime;
+
+public class MovieSessionResponseDto {
+    private Long id;
+    private String movieTitle;
+    private String cinemaHallId;
+    private String cinemaHallDescription;
+    private LocalDateTime showTime;
+    
+    public MovieSessionResponseDto() {
+    }
+    
+    public MovieSessionResponseDto(Long id,
+                                   String movieTitle,
+                                   String cinemaHallId,
+                                   String cinemaHallDescription,
+                                   LocalDateTime showTime) {
+        this.id = id;
+        this.movieTitle = movieTitle;
+        this.cinemaHallId = cinemaHallId;
+        this.cinemaHallDescription = cinemaHallDescription;
+        this.showTime = showTime;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+    
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+    
+    public String getCinemaHallId() {
+        return cinemaHallId;
+    }
+    
+    public void setCinemaHallId(String cinemaHallId) {
+        this.cinemaHallId = cinemaHallId;
+    }
+    
+    public String getCinemaHallDescription() {
+        return cinemaHallDescription;
+    }
+    
+    public void setCinemaHallDescription(String cinemaHallDescription) {
+        this.cinemaHallDescription = cinemaHallDescription;
+    }
+    
+    public LocalDateTime getShowTime() {
+        return showTime;
+    }
+    
+    public void setShowTime(LocalDateTime showTime) {
+        this.showTime = showTime;
+    }
+    
+    @Override
+    public String toString() {
+        return "MovieSessionResponseDto{"
+               + "id=" + id
+               + ", movieTitle='" + movieTitle + '\''
+               + ", cinemaHallId='" + cinemaHallId + '\''
+               + ", cinemaHallDescription='" + cinemaHallDescription + '\''
+               + ", showTime=" + showTime
+               + '}';
+    }
+}
