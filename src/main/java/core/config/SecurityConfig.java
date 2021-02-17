2957e1d65907744d7b063644f40c1fe8794ba482
Oleh Kravets <oleg.kravets92@gmail.com>
2021-02-17 18:16:11 +0200

    Spring security configured

--- src/main/java/core/config/SecurityConfig.java
+++ src/main/java/core/config/SecurityConfig.java
@@ -1,15 +1,34 @@
 package core.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Autowired
-    public void globalConfig(AuthenticationManagerBuilder managerBuilder) {
+    public void globalConfig(AuthenticationManagerBuilder managerBuilder) throws Exception {
+        managerBuilder.inMemoryAuthentication()
+                .withUser("user").password(getEncoder().encode("1234")).roles("user");
+    }
     
+    protected void config(HttpSecurity security) throws Exception {
+        security.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .httpBasic();
+    }
+    
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
